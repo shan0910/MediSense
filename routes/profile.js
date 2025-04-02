e97516f65138ad0c4fb114160f8eb667e97de334
Sudarshan Priye <emailsudarshanpriye@gmail.com>
2025-04-02 13:03:26 +0530

    fixed error

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getUserProfile } = require('../controllers/userController'); // Correct import
 const { authenticate } = require('../middleware/authMiddleware');
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.get('/me', authenticate, getUserProfile);
 // router.get('/me', authMiddleware, async (req, res) => {
